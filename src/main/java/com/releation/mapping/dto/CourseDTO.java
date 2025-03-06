1878c1edf68697345b8d5c771052b960f7eac21b
Asif Khan <asifkl786@gmail.com>
2025-03-06 12:45:06 +0530

    Add Lombok functionality

--- src/main/java/com/releation/mapping/dto/CourseDTO.java
+++ src/main/java/com/releation/mapping/dto/CourseDTO.java
@@ -11,38 +11,6 @@ public class CourseDTO {
     private Integer price;
     private String duration;
     private List<Long> studentIds;
-    
-    
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public Integer getPrice() {
-		return price;
-	}
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
-	public String getDuration() {
-		return duration;
-	}
-	public void setDuration(String duration) {
-		this.duration = duration;
-	}
-	public List<Long> getStudentIds() {
-		return studentIds;
-	}
-	public void setStudentIds(List<Long> studentIds) {
-		this.studentIds = studentIds;
-	}
-      
+   
 }
 
--- src/main/java/com/releation/mapping/dto/StudentDTO.java
+++ src/main/java/com/releation/mapping/dto/StudentDTO.java
@@ -11,38 +11,6 @@ public class StudentDTO {
     private String email;
     private String gender;
     private List<Long> courseIds;
-    
-    
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public List<Long> getCourseIds() {
-		return courseIds;
-	}
-	public void setCourseIds(List<Long> courseIds) {
-		this.courseIds = courseIds;
-	}
-    
+   
 }
 
